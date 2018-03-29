5662c0fa4c43d94ea539a2a1694a4ac3e5b42c53
Sarah Lynch <selynch@us.ibm.com>
2018-03-29 15:19:16 -0500

    commenting out address/geocode/location-related logic for now

--- bot.js
+++ bot.js
@@ -8,13 +8,13 @@ const PropertiesReader = require('properties-reader');
 const _landmark_addresses = PropertiesReader('wcs_config/raw/strings/landmark_addresses');
 
 
-const _control = require('./custom_classifiers/control');
-const _address = require('./custom_classifiers/addresses/features');
-const _cs = require('./custom_classifiers/crossstreets/features');
-const _ny_streets = require('./custom_classifiers/lib/ny_street.js');
-const _geocode = require('./lib/geocode.js');
-const _geonames = require('./lib/geonames.js');
-const _address_uninitialized = require('./wcs_config/dialog')._address_uninitialized;
+//const _control = require('./custom_classifiers/control');
+//const _address = require('./custom_classifiers/addresses/features');
+//const _cs = require('./custom_classifiers/crossstreets/features');
+//const _ny_streets = require('./custom_classifiers/lib/ny_street.js');
+//const _geocode = require('./lib/geocode.js');
+//const _geonames = require('./lib/geonames.js');
+//const _address_uninitialized = require('./wcs_config/dialog')._address_uninitialized;
 
 
 const conversation = new _conversation({
@@ -40,10 +40,10 @@ const resolve_locations = (context, address, geo_address, log_level) => {
 };
 
 const _fields_and_val = {
-  'address': _address_uninitialized,
-  'cs': '',
-  'c_address': _address_uninitialized,
-  'c_cs': '',
+  //'address': _address_uninitialized,
+  //'cs': '',
+  //'c_address': _address_uninitialized,
+  //'c_cs': '',
   'failed_ct': 0,
   'failed_address': 0,
   'c_ct': '',
@@ -67,87 +67,87 @@ class Bot extends EventEmitter {
     super();
 
     this._bot_ready = false;
-    _ny_streets.init()
-    .then(() => {
+    //_ny_streets.init()
+    //.then(() => {
       this._bot_ready = true;
       this.emit('ready');
-    });
+    //});
   }
 
   // BEWARE - HACKY CODE BELOW - MODIFY AT YOUR OWN PERIL!
   // this is the best effort at working around misclassifications by WCS
   // blame Joe for what follows....
- 
+
   // true if the user input is more than half occupied by a landmark word
   // TODO - this is crap - all we do is check the length! not even the word! what!
-  userInputIsProbablyJustALandMark (resp) {
-    let input_len = resp['input']['text'].length; 
-    for (let entity of resp['entities']) {
-      if(entity['entity'] == 'landmark') {
-        let len = entity['location'][1] - entity['location'][0];
-        return (len / input_len) > 0.5;
-      }
-    }
-    return false;
-  }
+  // userInputIsProbablyJustALandMark (resp) {
+  //   let input_len = resp['input']['text'].length;
+  //   for (let entity of resp['entities']) {
+  //     if(entity['entity'] == 'landmark') {
+  //       let len = entity['location'][1] - entity['location'][0];
+  //       return (len / input_len) > 0.5;
+  //     }
+  //   }
+  //   return false;
+  // }
+  //
+  // // true if more than half of the user's input are also in the address
+  // userInputIsProbablyJustAnAddress (resp, context) {
+  //   let user_input_arr = resp['input']['text'].toLowerCase().split(" ");
+  //   let address_arr = context['address'].toLowerCase().split(" ");
+  //   let num_matches = 0;
+  //   for(let user_input of user_input_arr) {
+  //     if(address_arr.indexOf(user_input) > -1) {
+  //       num_matches++;
+  //     }
+  //   }
+  //   return (num_matches / user_input_arr.length) > 0.5;
+  // }
+  //
+  // shouldEmitReportFound (resp, context, geocode_thinks_its_an_address) {
+  //   let result = false;
+  //
+  //   let wcs_thinks_its_a_report = resp['intents'].length > 0 && resp['intents'][0]['intent'].match(/^report-*/);
+  //   let dialog_thinks_its_a_report = 'ct_friendly' in context && context['ct_friendly'] != '';
+  //
+  //   // :|
+  //   let wcs_is_really_confident_its_a_report = wcs_thinks_its_a_report && resp['intents'][0].confidence > 0.5;
+  //
+  //   let probably_landmark = this.userInputIsProbablyJustALandMark(resp);
+  //   let probably_address = this.userInputIsProbablyJustAnAddress(resp, context);
+  //
+  //   // so bare minimum requirement, wcs has to sort of think it might be a report
+  //   if (wcs_thinks_its_a_report) {
+  //     // because our dialog has it's own rules for guessing if something is really a report,
+  //     // we also need to make sure the dialog is in agreement with WCS
+  //     if (dialog_thinks_its_a_report) {
+  //
+  //       // and here's where things get terrible
+  //
+  //       // if we're really confident, ok go ahead - if we're not that confident
+  //       // BUT geocode thinks it's not an address, also go ahead since they can't both be wrong, right? right?
+  //       if(!geocode_thinks_its_an_address || wcs_is_really_confident_its_a_report) {
+  //
+  //         // last hurdle - and perhaps the most hacky of them all - did we manually detect low levels of
+  //         // locationy type stuff in the utterance? if not, go on ahead
+  //         if (!probably_landmark && !probably_address) {
+  //           result = true;
+  //         } else {
+  //           //console.log("hacky: not a report b/c probably a location");
+  //         }
+  //       } else {
+  //         //console.log("hacky: not a report b/c just not confident enough");
+  //       }
+  //     } else {
+  //       //console.log("hacky: not a report b/c dialog doesn't think so");
+  //     }
+  //   } else {
+  //     //console.log("hacky: not a report b/c WCS doesn't think so");
+  //   }
+  //
+  //   return result;
+  // }
 
-  // true if more than half of the user's input are also in the address
-  userInputIsProbablyJustAnAddress (resp, context) {
-    let user_input_arr = resp['input']['text'].toLowerCase().split(" ");
-    let address_arr = context['address'].toLowerCase().split(" ");
-    let num_matches = 0;
-    for(let user_input of user_input_arr) {
-      if(address_arr.indexOf(user_input) > -1) {
-        num_matches++;
-      }
-    }
-    return (num_matches / user_input_arr.length) > 0.5;
-  }
-  
-  shouldEmitReportFound (resp, context, geocode_thinks_its_an_address) {
-    let result = false;
-
-    let wcs_thinks_its_a_report = resp['intents'].length > 0 && resp['intents'][0]['intent'].match(/^report-*/);
-    let dialog_thinks_its_a_report = 'ct_friendly' in context && context['ct_friendly'] != '';
-
-    // :|
-    let wcs_is_really_confident_its_a_report = wcs_thinks_its_a_report && resp['intents'][0].confidence > 0.5;
-
-    let probably_landmark = this.userInputIsProbablyJustALandMark(resp);
-    let probably_address = this.userInputIsProbablyJustAnAddress(resp, context);
-
-    // so bare minimum requirement, wcs has to sort of think it might be a report
-    if (wcs_thinks_its_a_report) {
-      // because our dialog has it's own rules for guessing if something is really a report,
-      // we also need to make sure the dialog is in agreement with WCS
-      if (dialog_thinks_its_a_report) {
-      
-        // and here's where things get terrible
-       
-        // if we're really confident, ok go ahead - if we're not that confident
-        // BUT geocode thinks it's not an address, also go ahead since they can't both be wrong, right? right?
-        if(!geocode_thinks_its_an_address || wcs_is_really_confident_its_a_report) {
-
-          // last hurdle - and perhaps the most hacky of them all - did we manually detect low levels of
-          // locationy type stuff in the utterance? if not, go on ahead
-          if (!probably_landmark && !probably_address) {
-            result = true;
-          } else {
-            //console.log("hacky: not a report b/c probably a location");
-          }
-        } else {
-          //console.log("hacky: not a report b/c just not confident enough");
-        }
-      } else {
-        //console.log("hacky: not a report b/c dialog doesn't think so");
-      }
-    } else {
-      //console.log("hacky: not a report b/c WCS doesn't think so");
-    }
-
-    return result;
-  }
-  
   // BEWARE - HACKY CODE ABOVE - MODIFY AT YOUR OWN PERIL!
 
   handleEmit (resp_loc, previous_context, error_message) {
@@ -208,20 +208,20 @@ class Bot extends EventEmitter {
   preprocess (input, context, log_level) {
     return new Promise((resolve, reject) => {
       Promise.all([
-        _control.classify(input, _address, './addresses/gen/model'),
+        //_control.classify(input, _address, './addresses/gen/model'),
       ])
+      // .then(custom => {
+      //   let address = custom[0];
+      //   return Promise.all([
+      //     address.text != '' ? _geocode.eval(address.text) : Promise.resolve(null),
+      //     Promise.resolve(address),
+      //   ]);
+      // })
       .then(custom => {
-        let address = custom[0];
-        return Promise.all([
-          address.text != '' ? _geocode.eval(address.text) : Promise.resolve(null),
-          Promise.resolve(address),
-        ]);
-      })
-      .then(custom => {
-        let geo_address = custom[0];
-        let address = custom[1];
+        //let geo_address = custom[0];
+        //let address = custom[1];
 
-        let found_location = resolve_locations(context, address, geo_address, log_level);
+        //let found_location = resolve_locations(context, address, geo_address, log_level);
 
         let payload = {
           workspace_id: process.env.WORKSPACE_ID,
@@ -239,7 +239,7 @@ class Bot extends EventEmitter {
             if(process.env.ENV.indexOf("prod") == -1) {
               console.log("error:", util.inspect(err, false, null));
             } else if (err.hasOwnProperty('error')) {
-              message = err.error; 
+              message = err.error;
             } else {
               console.log("error: handling unknown error:", util.inspect(err, false, null));
             }
@@ -248,14 +248,15 @@ class Bot extends EventEmitter {
             if(log_level > 1) console.log("resp:", util.inspect(resp, false, null));
 
             let landmark_address = resp['entities'].find(e => e.entity == 'landmark');
-            if(landmark_address) {
-              _geocode.eval(_landmark_addresses.get(landmark_address.value))
-              .then(loc => {
-                resolve({ resp: resp, found_location: _geocode.getLatLng(loc) });
-              });
-            } else {
-              resolve({ resp: resp, found_location: found_location });
-            }
+            // if(landmark_address) {
+            //   _geocode.eval(_landmark_addresses.get(landmark_address.value))
+            //   .then(loc => {
+            //     resolve({ resp: resp, found_location: _geocode.getLatLng(loc) });
+            //   });
+            // } else {
+              //resolve({ resp: resp, found_location: found_location });
+              resolve({ resp: resp });  //** This line  temporarily replaces the above line - can be deleted after putting "found_location" logic back in
+            //}
           }
         });
       })
